a5117e9e8ae7df8eb89e2ddfd8ee7a7490d51f59
releafs <ha@releafs.co>
2024-10-16 08:41:28 +0200

    Update scriptx.py

--- scripts/scriptx.py
+++ scripts/scriptx.py
@@ -3,17 +3,22 @@ import os
 # Define the process directory path
 process_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', 'process')
 
+# List of specific files to delete
+files_to_delete = ['decrypted_data.csv', 'decrypted_data_with_binary.csv', 'merged_data_with_metadata.csv']
+
 # Ensure the process directory exists
 if os.path.exists(process_dir):
-    # Get the list of all files in the process directory
-    for filename in os.listdir(process_dir):
-        # If the file ends with .csv, remove it
-        if filename.endswith(".csv"):
-            file_path = os.path.join(process_dir, filename)
+    # Iterate over the list of specific files to delete
+    for filename in files_to_delete:
+        file_path = os.path.join(process_dir, filename)
+        # Check if the file exists and delete it
+        if os.path.exists(file_path):
             try:
                 os.remove(file_path)
                 print(f"Deleted: {file_path}")
             except Exception as e:
                 print(f"Failed to delete {file_path}. Reason: {e}")
+        else:
+            print(f"File not found: {file_path}")
 else:
     print(f"Process directory does not exist: {process_dir}")
