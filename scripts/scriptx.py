2c3068b7d7ba53e3de484eb155146e3618a647b3
releafs <ha@releafs.co>
2024-10-16 09:02:15 +0200

    Update scriptx.py

--- scripts/scriptx.py
+++ scripts/scriptx.py
@@ -1,8 +1,14 @@
 import os
 
-# Absolute path to the process directory
-SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))  # Get the directory of the current script
-PROCESS_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, '..', 'process'))  # Process directory
+# Define the directories
+SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
+ROOT_DIR = os.path.abspath(os.path.join(SCRIPT_DIR, '..'))
+PROCESS_DIR = os.path.join(ROOT_DIR, 'process')
+
+# Print statements for debugging
+print(f"SCRIPT_DIR: {SCRIPT_DIR}")
+print(f"ROOT_DIR: {ROOT_DIR}")
+print(f"PROCESS_DIR: {PROCESS_DIR}")
 
 # List of specific files to delete
 files_to_delete = ['decrypted_data.csv', 'decrypted_data_with_binary.csv', 'merged_data_with_metadata.csv']
@@ -12,6 +18,7 @@ if os.path.exists(PROCESS_DIR):
     # Iterate over the list of specific files to delete
     for filename in files_to_delete:
         file_path = os.path.join(PROCESS_DIR, filename)
+        print(f"Checking for file: {file_path}")
         # Check if the file exists and delete it
         if os.path.exists(file_path):
             try:
